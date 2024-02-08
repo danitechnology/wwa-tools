58e13a50a54b78374733e71f5055487c24f8b803
danitechnology <contact@danitechno.com>
2024-02-08 17:21:26 +0700

    WWA Tools

---
+++ index.js
@@ -0,0 +1,12 @@
+/*
+ * Information
+ * Creator / Developer: Dani Technology (Dani Ramdani) - FullStack Engineer
+ * Contact Creator / Developer: 0895 3416 24750 (WhatsApp), contact@danitechno.com (Email)
+*/
+
+/* Thanks to
+ * Dani Technology (Dani Ramdani) - FullStack Engineer (Creator / Developer)
+ * @whiskeysockets/baileys (Library "Baileys" provider)
+*/
+
+module.exports = require('./lib/wwa-tools.js');
